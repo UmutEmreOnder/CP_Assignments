91ddc1e599b683ece200c0c8686e9d9b1c215da5
Umut <umutemreonder@gmail.com>
2022-05-23 00:37:44 +0300

    Changes After Code Review

--- CP1/Assignment - 2/HW2_150119018.java
+++ CP1/Assignment - 2/HW2_150119018.java
@@ -21,9 +21,22 @@ public class HW2_150119018 {
 		}
 		
 		// Define a tax variable
-		double tax = 0;
+		double tax = calculateTax(income, year);
+
+
+		// Calculating the other parts
+		double newBalance = (int)((income - tax) * 100) / 100.0;
+		double realTax = (int)((tax * 100 / income) * 100) / 100.0;
 		
-		// Calculating the tax according the year 		
+		// Showing the result		
+		System.out.println("Income: " + income 
+				+ "\nTax amount: " + (int)(tax * 100) / 100.0 
+				+ "\nIncome after tax: " + newBalance 
+				+ "\nReal tax rate: " + realTax + "%");
+	}
+
+	public double calculateTax(int income, int year) {
+		double tax = 0;
 		switch (year) {
 			case 2020:
 				if (income < 22000) {
@@ -40,9 +53,9 @@ public class HW2_150119018 {
 				}
 				else {
 					tax = 191070 + ((income - 600000) * 40 /100);
-				}		
+				}
 				break;
-				
+
 			case 2019:
 				if (income < 18000) {
 					tax = income * 15 / 100;
@@ -58,9 +71,9 @@ public class HW2_150119018 {
 				}
 				else {
 					tax = 163460 + ((income - 500000) * 40 /100);
-				}		
+				}
 				break;
-				
+
 			case 2018:
 				if (income < 14800) {
 					tax = income * 15 / 100;
@@ -75,7 +88,7 @@ public class HW2_150119018 {
 					tax = 29280 + ((income - 120000) * 35 / 100);
 				}
 				break;
-				
+
 			case 2017:
 				if (income < 13000) {
 					tax = income * 15 / 100;
@@ -90,22 +103,14 @@ public class HW2_150119018 {
 					tax = 26950 + ((income - 110000) * 35 / 100);
 				}
 				break;
-				
+
 			default:
 				//If not any case fits, show an error message and stop the program
 				System.out.println("Undefined year value!");
 				System.exit(1);
 		}
-		
-		// Calculating the other parts
-		double newBalance = (int)((income - tax) * 100) / 100.0;
-		double realTax = (int)((tax * 100 / income) * 100) / 100.0;
-		
-		// Showing the result		
-		System.out.println("Income: " + income 
-				+ "\nTax amount: " + (int)(tax * 100) / 100.0 
-				+ "\nIncome after tax: " + newBalance 
-				+ "\nReal tax rate: " + realTax + "%");
+
+		return tax;
 	}
 
 }
